f97041adbe0712913a34802f357570d638ae0167
diegoitaliait <diego.lagosmorales@pagopa.it>
2022-08-24 17:37:11 +0200

    minor fix

--- azure-devops/iac/03_iac_messages.tf
+++ azure-devops/iac/03_iac_messages.tf
@@ -3,7 +3,7 @@ variable "messages_iac" {
     repository = {
       organization    = "pagopa"
       name            = "io-infra"
-      branch_name     = "main"
+      branch_name     = "refs/heads/main"
       pipelines_path  = ".devops"
       yml_prefix_name = "messages"
     }
