4e6deb901e271bad80a5a873231ad4eb67aa2875
Andrea Grillo <andrea.grillo@pagopa.it>
2025-07-15 16:50:21 +0200

    Replace ioweb Key Vault (#59)

--- io-backend-projects/ioweb/99_variables.tf
+++ io-backend-projects/ioweb/99_variables.tf
@@ -13,7 +13,7 @@ locals {
   azure_devops_org = "pagopaspa"
   domain           = "ioweb"
   location         = "westeurope"
-  location_short   = "weu"
+  location_short   = "itn"
 
   # 🔐 KV
   # dev_key_vault_azdo_name  = "${local.prefix}-d-azdo-weu-kv"
@@ -26,11 +26,11 @@ locals {
 
   # dev_key_vault_name  = "${local.prefix}-d-${local.domain}-kv"
   # uat_key_vault_name  = "${local.prefix}-u-${local.domain}-kv"
-  prod_key_vault_name = "${local.prefix}-p-${local.domain}-kv"
+  prod_key_vault_name           = "${local.prefix}-p-${local.location_short}-${local.domain}-kv-01"
+  prod_key_vault_resource_group = "${local.prefix}-p-${local.location_short}-auth-main-rg-01"
 
   # dev_key_vault_resource_group  = "${local.prefix}-d-${local.domain}-sec-rg"
   # uat_key_vault_resource_group  = "${local.prefix}-u-${local.domain}-sec-rg"
-  prod_key_vault_resource_group = "${local.prefix}-p-${local.domain}-sec-rg"
 
   # ☁️ VNET
   # dev_dns_zone_resource_group  = "${local.prefix}-d-vnet-rg"
