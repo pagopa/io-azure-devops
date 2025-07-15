24d16bb3cec87724c01e61ad2bd29bb9ab88555d
Andrea Grillo <andrea.grillo@pagopa.it>
2025-07-15 16:51:17 +0200

    Delete AKS things (#58)

--- io-backend-projects/messages/03_service_connections_sonarcloud.tf
+++
@@ -1,4 +0,0 @@
-# TODO azure devops terraform provider does not support SonarCloud service endpoint
-locals {
-  azuredevops_serviceendpoint_sonarcloud_id = "TODO: create sonar cloud id"
-}
--- io-backend-projects/profile/02_data.tf
+++
@@ -1,4 +0,0 @@
-data "azurerm_key_vault" "kv_prod" {
-  name                = local.prod_key_vault_name
-  resource_group_name = local.prod_key_vault_resource_group
-}
