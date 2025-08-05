4e315447c522d54cb2e2e4d08a8dc8d75a40cc80
Krusty93 <andrea.grillo@pagopa.it>
2025-08-05 17:55:18 +0200

    restore ioweb

--- io-backend-projects/ioweb/02_data.tf
+++ io-backend-projects/ioweb/02_data.tf
@@ -2,3 +2,8 @@ data "azurerm_key_vault" "kv_prod" {
   name                = local.prod_key_vault_name
   resource_group_name = local.prod_key_vault_resource_group
 }
+
+data "azurerm_key_vault" "ioweb_legacy" {
+  name                = "io-p-ioweb-kv"
+  resource_group_name = "io-p-ioweb-sec-rg"
+}
